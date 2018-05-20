d7857a04bb95636e363d4d5dcb142d3ab8700b59
montlikadani <montlikada@gmail.com>
2018-05-20 08:31:39 +0200

    Update Config.java

--- src/com/SirBlobman/staffchatx/Config.java
+++ src/com/SirBlobman/staffchatx/Config.java
@@ -59,6 +59,8 @@ public class Config {
     private static void defaults() {
         set("options.log to console", true, false);
         set("messages.format", "&5&lSTAFF &f{username}&r &7\u00BB&r {message}", false);
+        set("messages.read permission", "staffchat.read", false);
+        set("messages.send permission", "staffchat.send", false);
         set("messages.command.usage", "&f/<command> <toggle> &eOR &f/<command> <message...>", false);
         set("messages.command.not player", "&cYou are not a Player", false);
         set("messages.command.added", "&5Staff Chat: &2on", false);
